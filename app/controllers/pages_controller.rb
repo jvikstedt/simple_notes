17f09107b85f47fb539f9977cbaea9c0fdd34a84
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-10-30 22:29:55 +0200

    pages#create action and tests for it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,24 @@
 class PagesController < ApplicationController
   def index
-    @pages = Page.all
+    pages = Page.all
     respond_to do |format|
-      format.json  { render json: @pages }
+      format.json  { render json: pages }
     end
+  end
+
+  def create
+    page = Page.new(page_params)
+
+    if page.save
+      head :created
+    else
+      render json: page.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title, :body)
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe PagesController, type: :controller do
       expect(response.body).to eq(pages.to_json.to_s)
     end
   end
+
+  describe 'POST #create' do
+    it 'returns status created with valid parameters' do
+      post :create, format: :json, page: attributes_for(:page)
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'creates pages when called with valid parameters' do
+      expect do
+        post :create, format: :json, page: attributes_for(:page)
+      end.to change { Page.all.size }.by(1)
+    end
+
+    it 'returns status unprocessable_entity with invalid parameters' do
+      post :create, format: :json, page: attributes_for(:page, title: nil)
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
