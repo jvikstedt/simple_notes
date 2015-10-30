89f18b264c927fed6b20846c547d79e617161302
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-10-30 19:07:56 +0200

    Added configuration about test file generation, mainly to generate factory files automatically

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,18 @@ module SimpleNotes
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
+    # Testing generators setup
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
