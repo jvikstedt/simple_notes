80079976ecf20a6334cc4ba1a60643e2b047acb4
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-10-30 18:43:27 +0200

    Fixed simple_notes#index test after renaming stuff

--- spec/controllers/simple_notes_controller_spec.rb
+++ spec/controllers/simple_notes_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe SimpleNotesController, type: :controller do
   context 'root url' do
-    it 'renders home#index template' do
-      expect(get: root_url).to route_to( controller: "home", action: "index")
+    it 'renders simple_notes#index template' do
+      expect(get: root_url).to route_to( controller: "simple_notes", action: "index")
     end
   end
 end
