86b861646ac1bb25e4bd3a6a1e444a8c923943ae
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-10-30 20:07:04 +0200

    Modified PagesController #index spec to test for json data

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns json data of all pages' do
+      pages = [create(:page), create(:page)]
       get :index, format: :json
-      expect(response).to have_http_status(:success)
+      expect(response.body).to eq(pages.to_json.to_s)
     end
   end
 end
