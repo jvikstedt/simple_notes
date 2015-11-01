621fed95208ae3a33c3de58d1fc01eb9edc76409
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-11-01 17:34:52 +0200

    Moved pages json api under api/v1 route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'simple_notes#index'
 
-  resources :pages, only: [:index, :show, :create, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :pages, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
